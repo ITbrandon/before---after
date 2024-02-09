c12fc873e9b4f6472e73bc5ad24775d83ce8f379
Itbrandon <brandoniticka@gmail.com>
2024-02-08 17:40:24 -0800

    update

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
 //Number
+let beforeNumberVariable = document.querySelector('#before-number-value');
 const numberVariable = document.querySelector('#number-value');
 
 let initialNumber = 5.125456;
+
+beforeNumberVariable.textContent = initialNumber
 
 initialNumber++;
 
@@ -12,9 +15,12 @@ fixedNumber.toString();
 numberVariable.textContent = fixedNumber;
 
 //String
+let beforeStringVariable = document.querySelector('#before-string-value');
 const stringVariable = document.querySelector('#string-value');
 
 let initialString = 'Hello, World';
+
+beforeStringVariable.textContent = initialString;
 
 let newString = initialString.replace('World', 'People');
 
@@ -23,9 +29,12 @@ const finalString = newString.slice(7);
 stringVariable.textContent = finalString;
 
 //Array
+let beforeArrayVariable = document.querySelector('#before-array-value');
 const arrayVariable = document.querySelector('#array-value');
 
 const initialArray = [1, 2, 3, 4, 5];
+
+beforeArrayVariable.textContent = initialArray;
 
 initialArray.push(6);
 
@@ -34,9 +43,14 @@ initialArray.shift();
 arrayVariable.textContent = initialArray;
 
 //Object
+let beforeObjectVariable = document.querySelector('#before-object-value');
 const objectVariable = document.querySelector('#object-value');
 
 let initialObject = { name: "Brandon", age: 21 };
+
+beforeObjectVariable.textContent = Object.values(initialObject);
+
+initialObject.name = "Boston";
 
 initialObject.favoriteFood = "Pizza";
 
